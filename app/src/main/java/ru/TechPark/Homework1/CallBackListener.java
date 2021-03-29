286e98d0079e927fe67df227863a4739274516e4
AnDrEy117722 <58273707+AnDrEy117722@users.noreply.github.com>
2021-03-29 16:55:42 +0300

    "V2.1"

---
+++ app/src/main/java/ru/TechPark/Homework1/CallBackListener.java
@@ -0,0 +1,9 @@
+package ru.TechPark.Homework1;
+
+import android.widget.Button;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface CallBackListener {
+    void onCallBack(RecyclerView recyclerView, Button button);
+}
--- app/src/main/java/ru/TechPark/Homework1/MainActivity.java
+++ app/src/main/java/ru/TechPark/Homework1/MainActivity.java
@@ -1,17 +1,25 @@
 package ru.TechPark.Homework1;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements CallBackListener {
+
+    List<NumbersModel> number = NumberSource.getInstance().getRemoteData();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,4 +51,16 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+    @Override
+    public void onCallBack(RecyclerView recyclerView, Button addNumber) {
+        int color;
+        if ((number.size() + 1) % 2 == 0) {
+            color = Color.BLUE;
+        } else {
+            color = Color.RED;
+        }
+        number.add(new NumbersModel(number.size() + 1, color));
+        recyclerView.getAdapter().notifyItemInserted(number.size()-1);
+        recyclerView.smoothScrollToPosition(number.size()-1);
+    }
 }
